602b9fc25bcbbcea1445234409ae45bf0ea72e3e
bayoudhali44 <bayoudhali44@gmail.com>
2024-09-22 19:03:48 +0200

    feat: 🎸 Cors
    configure Cors

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import routeRoutes from "./src/routes/route.routes";
 
-// Load environment variables from .env
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON
+app.use(cors());
+
 app.use(express.json());
 
-// Use the route routes
 app.use("/api", routeRoutes);
 
 app.listen(port, () => {
